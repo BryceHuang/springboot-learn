3347230f5acc1ba1ea20af4717e42d1a862cb633
BryceHuang <18801097781@163.com>
2019-04-16 22:05:24 +0800

    删掉了多数据源的配置

--- src/main/java/com/example/SpringbootApplication.java
+++ src/main/java/com/example/SpringbootApplication.java
@@ -9,10 +9,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = "com.example.repository")
 @EntityScan(basePackages = "com.example.entity")
-public class SessionTestApplication {
+public class SpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SessionTestApplication.class, args);
+        SpringApplication.run(SpringbootApplication.class, args);
     }
 
 }
